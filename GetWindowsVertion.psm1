e3bcf4f0945264d40e240e6a3b9510ff2270e3c2
Syuichi Murashima <mura-github@vwnet.jp>
2019-08-02 11:14:55 +0900

    1st commit

---
+++ GetWindowsVertion.psm1
@@ -0,0 +1,34 @@
+﻿#########################################
+# Windows ビルド番号取得
+#########################################
+function GetWindowsVertion(){
+    # ビルド番号詳細
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    $RegKey = "BuildLabEx"
+    echo (Get-ItemProperty $RegPath -name $RegKey).$RegKey
+
+    # Winver のビルド番号
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    $RegKey = "CurrentBuild"
+    $MajorNumber = (Get-ItemProperty -Path $RegPath -name $RegKey).$RegKey
+    $RegKey = "UBR"
+    $MinorNumber = (Get-ItemProperty -Path $RegPath -name $RegKey).$RegKey
+    $BuildNumber = $MajorNumber + "." + [String]$MinorNumber
+    echo $BuildNumber
+
+    # Winver のバージョン
+    $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+    $RegKey = "ReleaseId"
+    echo (Get-ItemProperty $RegPath -name $RegKey -ErrorAction SilentlyContinue).$RegKey
+
+    # Hot Fix
+    Get-HotFix | sort InstalledOn -Descending
+
+    # 起動時刻
+    $WMI_OpreationSystem = Get-WmiObject win32_operatingsystem
+    $Boot = $WMI_OpreationSystem.LastBootUpTime
+    $WMI_OpreationSystem.ConvertToDateTime($Boot)
+}
+
+
+
---
+++ ReadMe.txt
@@ -0,0 +1,30 @@
+﻿Windows のビルド番号を取得する
+
+■ これは何?
+Windows のバージョン、ビルド番号を表示します
+
+■ 使い方
+PowerShell プロンプトで以下コマンド入力
+
+GetWindowsVertion
+
+■ セットの仕方
+PowerShell プロンプトで install.ps1 を実行してください
+
+次に PowerShell プロンプト開くと apo コマンドが使えるようになっています。
+
+■ Uninstall 方法
+uninstall.ps1 を実行して下さい
+(問い合わせが来たら Enter)
+
+■ 動作確認環境
+PowerShell 5.1
+Windows 環境専用です。
+
+■ Web サイト
+Windows のビルド番号詳細を確認する
+http://www.vwnet.jp/Windows/w10/Check/BuildNo.htm
+
+■ リポジトリ
+https://github.com/MuraAtVwnet/GetWindowsVertion
+git@github.com:MuraAtVwnet/GetWindowsVertion.git
